7a6a561ed1c2be79b3eb2fdb4bbb2a2ca5bf9afd
zoton2 <zoton2@gmail.com>
2021-02-04 19:25:33 +0000

    Hardcode production "assetsPublicPath" config
    I'm not sure how to easily do it with config/env vars although this really seems like a config file, just buried too deep.

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../../dist/frontend'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/mods/',
 
     /**
      * Source Maps
